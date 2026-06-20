ad3371453d6b608f0de582c5908eccd8c320b805
SabaaSandhu <sabaasandhu789@gmail.com>
2026-06-20 16:59:38 -0700

    fix railway deploy

--- server/settings.py
+++ server/settings.py
@@ -17,7 +17,10 @@ from corsheaders.defaults import default_headers
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-import dj_database_url 
+try:
+    import dj_database_url
+except ImportError:
+    dj_database_url = None
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
