a546d9b18657047330ca49cf1d7de87e7d428da5
SabaaSandhu <sabaasandhu789@gmail.com>
2026-06-20 17:18:13 -0700

    fix database url package

--- server/settings.py
+++ server/settings.py
@@ -17,10 +17,7 @@ from corsheaders.defaults import default_headers
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-try:
-    import dj_database_url
-except ImportError:
-    dj_database_url = None
+import dj_database_url 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
