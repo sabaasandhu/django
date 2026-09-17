bdb3d46bf87a7690af04fc35a15cf2f79182c9b5
SabaaSandhu <sabaasandhu789@gmail.com>
2026-09-17 16:38:44 -0700

    Final fix

--- server/settings.py
+++ server/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 ROOT_URLCONF = 'server.urls'
 
 TEMPLATES = [
