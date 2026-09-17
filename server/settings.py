ca8124f372405c67a56b3d53c5a563d4e70647fd
SabaaSandhu <sabaasandhu789@gmail.com>
2026-09-17 16:29:56 -0700

    Use CompressedStaticFilesStorage instead of Manifest

--- server/settings.py
+++ server/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 ROOT_URLCONF = 'server.urls'
 
 TEMPLATES = [
@@ -265,6 +265,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",  
     },
+
 }
